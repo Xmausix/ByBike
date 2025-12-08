a187d8b31f3b58de9b025ca468ea9f6b7b22ef59
Xmausix <barbara.szymanska469@gmail.com>
2025-12-08 18:41:59 +0100

    Switch to Vercel adapter for SvelteKit deployment
    Replaces @sveltejs/adapter-node with @sveltejs/adapter-vercel in svelte.config.js and adds @sveltejs/adapter-vercel to dependencies. Introduces vercel.json for deployment configuration, enabling deployment to Vercel platform.

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,16 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel'; // ZMIANA TUTAJ
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter()
-  }
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			// Opcjonalne konfiguracje
+			// runtime: 'edge',
+			// split: true
+		})
+	}
 };
 
 export default config;
